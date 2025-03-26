b1c10638a45abc68cd201e15ffafaae6cae8378a
Pedro Perez <pedropm.lolo05@gmail.com>
2025-03-26 04:14:12 +0100

    cambio de extensiones

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import GameCard from "./GameCard";
 
 function GameList({ games }) {
-  console.log(games); // Verifica si los datos están siendo pasados correctamente
-
   return (
     <div className="game-list">
       {games.map((game) => (
